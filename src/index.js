ab4c9121fbd5804307503b8b27b02fd5a6370332
Ian Cottam <ian.cottam@bbc.co.uk>
2018-12-19 15:36:08 +0000

    [IC] - Basic set up

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import constants from './config/constants';
+import './config/database';
+import apiRoutes from './modules';
+import middlewareConfig from './config/middleware';
+const app = express();
+middlewareConfig(app);
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+apiRoutes(app);
+app.listen(constants.PORT, err => {
+  if (err) {
+      throw err;
+  } else {
+      console.log(`
+      Server running on port: ${constants.PORT}
+      --- 
+      Running on ${process.env.NODE_ENV}
+      --- 
+      Make something great!`)
+  }
+});
